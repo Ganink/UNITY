105bb0c06c3e8c097e87ea042ef02dfa7fdc9090
xtröd <christiancaleroerazo@gmail.com>
2017-07-02 19:06:03 +0200

    Add files via upload

---
+++ Pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    bool active;
+    Canvas canvas;
+
+	void Start () {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)){
+            //conmutamos si está entre activo o no.
+            active = !active;
+            canvas.enabled = active;
+            /*para detener el juego completamente
+            si está pausado entra por el 0 que detiene el juego.
+            si no, entra por el 1 y sigue su velocidad normal*/
+            Time.timeScale = (active) ? 0 : 1f;
+        }
+	}
+}
